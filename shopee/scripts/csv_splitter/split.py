19779e2dc0bdbdb1bf2923e1d75123088c164722
Ferdi Riansyah <Rian>
2025-04-27 20:43:43 +0700

    fix: duplicated customer data

--- shopee/scripts/csv_splitter/split.py
+++ shopee/scripts/csv_splitter/split.py
@@ -5,6 +5,7 @@ import os
 import pandas as pd
 import csv
 
+
 def parseArgs():
     if len(sys.argv) != 2:
         print(f"Usage: python3 {sys.argv[0]} <input_file>.csv")
@@ -12,14 +13,17 @@ def parseArgs():
     fname = sys.argv[1]
     return fname
 
+
 def loadCSV(fname):
-    df = pd.read_csv(fname, delimiter=';', dtype=str)
+    df = pd.read_csv(fname, delimiter=",", dtype=str)
     return df
 
+
 def produceCSV(df, fout):
-    df.to_csv(fout, sep=';', index=False, quoting=csv.QUOTE_ALL)
+    df.to_csv(fout, sep=";", index=False, quoting=csv.QUOTE_ALL)
     print(f"Output saved to {fout}")
     return
+
 
 def produceSelectiveCSV(data, keys, fout, dedupe=False):
     df = pd.DataFrame(data, columns=keys)
@@ -28,9 +32,11 @@ def produceSelectiveCSV(data, keys, fout, dedupe=False):
     produceCSV(df, fout)
     return
 
+
 def renameKeys(df, keymap):
     df = df.rename(columns=keymap)
     return df
+
 
 def main():
     fname = parseArgs()
@@ -40,38 +46,78 @@ def main():
     os.makedirs(dirname, exist_ok=True)
     os.chdir(dirname)
 
-    # produce customer data    
+    # produce customer data
     # Rename Customer Keys
-    customerData = renameKeys(df, {
-        "Alamat Pengiriman": "Alamat",
-        "Kota/Kabupaten": "Kota",
-    })
-    produceSelectiveCSV(customerData, ["Username (Pembeli)", "Nama Penerima", "No. Telepon", "Kota", "Alamat", "Provinsi"], "customer.csv", dedupe=True)
+    customerData = renameKeys(
+        df,
+        {
+            "Alamat Pengiriman": "Alamat",
+            "Kota/Kabupaten": "Kota",
+        },
+    )
+    produceSelectiveCSV(
+        customerData,
+        [
+            "Username (Pembeli)",
+            "Kota",
+            "Alamat",
+            "Provinsi",
+        ],
+        "customer.csv",
+        dedupe=True,
+    )
 
     # produce transaction order data
     # Rename Transaction Order Group Keys
-    transactionOrderGroupData = renameKeys(df, {
-        "No. Pesanan": "Transaction Code",
-        "Waktu Pesanan Dibuat": "Transaction Date",
-        "Username (Pembeli)": "Customer",
-    })
+    transactionOrderGroupData = renameKeys(
+        df,
+        {
+            "No. Pesanan": "Transaction Code",
+            "Waktu Pesanan Dibuat": "Transaction Date",
+            "Username (Pembeli)": "Customer",
+        },
+    )
     transactionOrderGroupData["Name"] = transactionOrderGroupData["Transaction Code"]
     transactionOrderGroupData["Marketplace"] = "Shopee"
-    produceSelectiveCSV(transactionOrderGroupData, ["Name", "Transaction Code", "Marketplace", "Customer", "Transaction Date"], "transaction_order_group.csv", dedupe=True)
-    
+    produceSelectiveCSV(
+        transactionOrderGroupData,
+        ["Name", "Transaction Code", "Marketplace", "Customer", "Transaction Date"],
+        "transaction_order_group.csv",
+        dedupe=True,
+    )
+
     # produce transaction order list data
     # Rename Transaction Order List Keys
-    transactionOrderListData = renameKeys(df, {
-        "No. Pesanan": "Transaction Code",
-        "Waktu Pesanan Dibuat": "Transaction Date",
-        "Nomor Referensi SKU": "Product",
-        "Harga Setelah Diskon": "Price (unit)",
-        "Jumlah": "Qty",
-    })
+    transactionOrderListData = renameKeys(
+        df,
+        {
+            "No. Pesanan": "Transaction Code",
+            "Waktu Pesanan Dibuat": "Transaction Date",
+            "Nomor Referensi SKU": "Product",
+            "Harga Setelah Diskon": "Price (unit)",
+            "Jumlah": "Qty",
+        },
+    )
 
-    transactionOrderListData['Name'] = transactionOrderListData["Transaction Code"]
-    transactionOrderListData["Price (unit)"] = transactionOrderListData["Price (unit)"].str.replace(".", "")
-    produceSelectiveCSV(transactionOrderListData, ["Name", "Transaction Code", "Transaction Date", "Product", "Price (unit)", "Qty"], "transaction_order_list.csv", dedupe=True)
+    transactionOrderListData["Name"] = transactionOrderListData["Transaction Code"]
+    transactionOrderListData["Price (unit)"] = transactionOrderListData[
+        "Price (unit)"
+    ].str.replace(".", "")
+    transactionOrderListData["Product Code"] = transactionOrderListData["Product"]
+    produceSelectiveCSV(
+        transactionOrderListData,
+        [
+            "Name",
+            "Transaction Code",
+            "Transaction Date",
+            "Product Code",
+            "Price (unit)",
+            "Qty",
+        ],
+        "transaction_order_list.csv",
+        dedupe=True,
+    )
+
 
 if __name__ == "__main__":
     main()
